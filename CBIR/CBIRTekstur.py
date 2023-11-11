d6103773b3ce368f4f4aa397cdc36f24b827ed2e
Farhannr28 <farhannafis281004@gmail.com>
2023-11-11 10:53:18 +0700

    feat: added coocurence Matrix process

--- CBIR/CBIRTekstur.py
+++ CBIR/CBIRTekstur.py
@@ -1,13 +1,40 @@
+from math import *
 import numpy as np
-import math 
 
+def convertGrayscale(R, G, B):
+    return 0.29 * R + 0.587 * G + 0.114 * B
 
-def RGBtoGrayscale(R,G,B):
-    return 0.29*R + 0.587*G + 0.114*B
+def cooccurenceMatrix(matrix, angle):
+    res = np.zeros((256,256))
+    x = cos(angle)
+    y = sin(angle)
 
+    if (x < 0.5):
+        x = 0
+    else:
+        x = 1
 
-def matrixNorm(matrixOcc):
-    pass
+    if (y < 0.5):
+        y = 0
+    else:
+        y = 1
+        
+    size = matrix.shape
+    for i in range(size[0] - y): # i + y < size[]
+        for j in range(size[1] - x): # j + x < size[]
+            row = matrix[i,j]
+            col = matrix[i+y,j+x]
+            res[row, col] += 1
+    return res    
+
+def symmetricMatrix(matrixCo):
+    t = matrixCo.transpose()
+    res = t + matrixCo
+    return res
+
+def matrixNorm(matrixSym):
+    sum = matrixSym.sum()
+    return (matrixSym/sum)
 
 #menghitung contrast pada matrix
 def contrast():
@@ -22,4 +49,3 @@ def entropy():
 #mencari tingkat kemiripan dengan cosine similarity
 def cosine_similarity(a,b):
     return np.dot(a,b)/(np.linalg(a)*np.linalg(b))
-
