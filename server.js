ecf77c5ae7f58fe35a98a48363c957e1cc140b69
mrsuiii <emeryfathanzwageri@gmail.com>
2023-11-17 20:53:31 +0700

    feat : add server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const fs = require("fs");
+
+
+
+app.use(express.static("./client"));
+const uploadClientPath = "./uploads/client_image";
+const uploadDatasetPath = "./uploads/dataset"
+const datasetStorage = multer.diskStorage({
+  destination: (req,file,cb) => {
+    if(!fs.existsSync(uploadDatasetPath)){
+      fs.mkdirSync(uploadDatasetPath,{recursive: true})
+    }
+    cb(null, uploadDatasetPath)
+  },filename: (req,file,cb)=>{
+    cb(null,file.originalname)
+  }
+})
+const clientStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Dapatkan nama subfolder dari timestamp saat ini
+    // Cek apakah folder sudah ada atau belum, jika belum, buat folder baru
+    if (!fs.existsSync(uploadClientPath)) {
+      fs.mkdirSync(uploadClientPath, { recursive: true });
+    }
+    cb(null, uploadClientPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const clientUpload = multer({ storage: clientStorage });
+const datasetUpload = multer({storage : datasetStorage})
+app.post("/api/submit",clientUpload.array("inputImg") ,(req, res) => {
+  console.log(`req is ${req}`);
+  console.log("hello world!");
+  app.post("/api/dataset",datasetUpload.array("inputDataset"),(req,res)=>{
+    res.status(200).json({ message: "Files uploaded successfully!" });
+    
+  })
+  
+  const toggleValue = req.body["toggleStatus"]
+  console.log(`toggle value: ${toggleValue}`);
+  res.status(200).json({ message: "Files uploaded successfully!" });
+});
+
+
+app.listen(5001, () => {
+  console.log("Server is running on http://localhost:5001");
+});
+
+
