9975ce74eada4556495d6abf601d96afc7b8a234
Farhannr28 <farhannafis281004@gmail.com>
2023-11-18 21:11:27 +0700

    CBIR Color optimization

--- CBIR/CBIRWarna.py
+++ CBIR/CBIRWarna.py
@@ -2,37 +2,9 @@ import numpy as np
 from PIL import Image
 from math import *
 
+"""
 def Norm(a):
     return a/255
-
-"""
-def Max(a, b, c):
-    if (a>b):
-        if (a>c):
-            return a
-        else:
-            return c
-    else:
-        if (b>c):
-            return b
-        else:
-            return c 
-            
-def Min(a, b, c):
-    if (a<b):
-        if (a<c):
-            return a
-        else:
-            return c
-    else:
-        if (b<c):
-            return b
-        else:
-            return c 
-
-def Delta(R,G,B):
-    return Cmax(R,G,B)-Cmin(R,G,B)
-"""
 
 def Hue(R,G,B, Cmax, Delta):
     if (Delta == 0):
@@ -50,35 +22,27 @@ def Sat(Cmax, Delta):
     else:
         return Delta/Cmax
 
-"""
-def Val(Cmax):
-    return Cmax
-"""
-
-"""
-def RGBtoHSV(R,G,B,size):
-    H = np.zeros((size,size))
-    S = np.zeros((size,size))
-    V = np.zeros((size,size))
-    for i in range(size):
-        for j in range(size):
-            newR = Norm(R[i , j])
-            newG = Norm(G[i , j])
-            newB = Norm(B[i , j])
-            Cmin = min(newR,newG,newB)
-            Cmax = max(newR,newG,newB)
-            Delta = Cmin - Cmax
-            H[i , j] = Hue(newR, newG, newB, Cmax, Delta)
-            S[i , j] = Sat(Cmax, Delta)
-            V[i , j] = Cmax
-    return H,S,V
-"""
-
 def HSVPixel(R,G,B):
     R /= 225
     G /= 225
     B /= 225
     Cmin = min(R,G,B)
     Cmax = max(R,G,B)
-    Delta = Cmin - Cmax
+    Delta = Cmax - Cmin
     return np.array([Hue(R,G,B,Cmax,Delta), Sat(Cmax, Delta), Cmax])
+"""
+
+def RGBtoHSV(arr):
+    arr = arr / 225
+    Val = np.max(arr, axis=-1) #Cmax
+    Cmin = np.min(arr, axis=-1)
+    Delta = Val - Cmin
+    Sat = np.where(Val == 0, 0, np.divide(Delta, Val, where=Val!=0))
+    R = arr[:,:,0]
+    G = arr[:,:,1]
+    B = arr[:,:,2]
+    Hue = np.where(Delta == 0, 0, 
+        np.select([Val == R, Val == G, Val == B], [np.divide(((pi/3)*(G - B)), Delta, where=Delta!=0)%6, 
+                                                   np.divide(((pi/3)*(B - R)), Delta, where=Delta!=0)+2, 
+                                                   np.divide((pi/3)*(R - G), Delta, where=Delta != 0)+4]))
+    return np.stack([Hue, Sat, Val], axis = -1)
